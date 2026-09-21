5b4d58a06fc7de574956d232d11ce3e378b46a73
Sanjay70100 <sanjaymaster9751@gmail.com>
2026-09-21 15:38:51 +0530

    trainingday1

---
+++ excercise.java
@@ -0,0 +1,23 @@
+public class excercise {
+    public static void main(String[] args) {
+        String name="sanjay";
+        int age=21;
+        float height=5.6f;
+        double weight=50;
+        int a=15;
+        int b=25;
+        float c=(float)a/b;
+        String city="chennai";
+        int pincode=600001;
+        System.out.println("name:" + name);
+        System.out.println("age:" + age);
+        System.out.println("height:" + height);
+        System.out.println("weight:" + weight);
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(c);
+        System.out.println("city:" + city);
+        System.out.println("pincode:" + pincode);
+    }
+}
---
+++ hello.java
@@ -0,0 +1,21 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        byte a=12;
+        int b=1;
+        short c=4;
+        long d=7;
+        float e=100;
+        double f=108;
+        boolean g=a>b;
+        char h='v';
+        System.out.println("a:" + a);
+        System.err.println("b:" + b);
+        System.out.println("c:" + c);
+        System.out.println("d:" + d);
+        System.out.println("e;" + e);
+        System.out.println("f:" + f);
+        System.out.println("g;" + g);
+        System.out.println("h:" + h);
+    }
+}
